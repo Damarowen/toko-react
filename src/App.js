fbecc429811d5f54c6e34c902320c4b385eb6122
damarowen <damarowen@gmail.com>
2021-02-05 17:09:49 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 import {
-  BrowserRouter as Router, Route, Switch
+  BrowserRouter as Route, Switch
 } from "react-router-dom";
 
 
 
 function App() {
-  
+
   return (
-    <Router>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Router>
+
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+
 
   );
 }
