073cd2ca7bbd49113048ca31716316e5a989e87d
Damarowen <67860422+Damarowen@users.noreply.github.com>
2021-03-17 23:07:47 +0700

    stripe error

--- client/src/components/StripeButton/StripeButton.js
+++ client/src/components/StripeButton/StripeButton.js
@@ -1,47 +1,44 @@
-import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
-const StripeCheckoutButton = ({ harga }) => {
-    const stripePrice = harga * 100;
-    const publicshableKey = 'pk_test_51ILRHfLUkFFNf0MFWyVrEmjHCWGf7ouiIY13HU6ZwUZqnanhbQ9uKQY7pGCHcFZ7mfpSueRMqeCE3Samsqlc6u1j00mkP3kiXn'
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
 
-console.log(harga)
-    //* on success
-    const onToken = token => {
-        axios({
-          url: 'payment',
-          method: 'post',
-          data: {
-            amount: stripePrice,
-            token: token
-          }
-        })
-          .then(response => {
-            alert('succesful payment');
-          })
-          .catch(error => {
-            console.log('Payment Error: ', error);
-            alert(
-              'There was an issue with your payment! Please make sure you use the provided credit card.'
-            );
-          });
-      };
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
 
-    return (
-        <StripeCheckout
-            label='Pay Now'
-            name='Damar Clothin ltd.'
-            billingAddress
-            shippingAddress
-            image='https://seeklogo.com/images/S/stripe-logo-C409DC9652-seeklogo.com.png'
-            description={`Your total is ${harga}`}
-            amount={stripePrice}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publicshableKey}
-        />
-        
-    )
-}
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
